c084d04d7e6f420a8fcf950dcfbad68ec5a45090
Charles Dickens <charlieadickens@gmail.com>
2021-04-05 15:52:47 -0700

    Update MemoryVariableTermStore.java

--- psl-core/src/main/java/org/linqs/psl/reasoner/term/MemoryVariableTermStore.java
+++ psl-core/src/main/java/org/linqs/psl/reasoner/term/MemoryVariableTermStore.java
@@ -27,9 +27,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
